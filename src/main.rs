1a65ad5057220ddf132d778ef7f732987310ecc9
Usern-4me <mazurkiewicz.leon@gmail.com>
2025-07-09 15:39:35 +0200

    Add pattern to ignore enviroment files2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();  // Load environment variables from .env file
+    let api_key = env::var("OPENAI_API_KEY").expect("Key not set in .env file");
+
+    print!("api key: {}", api_key);
+
+
+    
 }
